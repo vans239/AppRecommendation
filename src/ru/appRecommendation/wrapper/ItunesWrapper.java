71d1810729a818e5b19976de3735e2ee15fba1be
Evgeny Vanslov <vans239@gmail.com>
2011-10-14 11:26:31 +0400

    Wrapper of itunes.com was done.

--- src/ru/appRecommendation/wrapper/ItunesWrapper.java
+++ src/ru/appRecommendation/wrapper/ItunesWrapper.java
@@ -12,11 +12,12 @@ import ru.appRecommendation.core.Wrapper;
 import ru.appRecommendation.exception.WebHarvestException;
 
 public class ItunesWrapper extends Wrapper {
-	private static Logger log = Logger.getLogger(ItunesWrapper.class);
-	private String configPath = "resources/webharvest/downloader/downloadAllLinks.xml";
-	private String tempPath = "data";
-	private String linksFilePath = tempPath + "/" + "allLinks.txt";
-	private String workingDir = System.getProperty("user.dir");
+	private final static Logger log = Logger.getLogger(ItunesWrapper.class);
+	private final String wrapLinkConfigPath = "resources/webharvest/downloader/downloadAllLinks.xml";
+	private final String wrapPageConfigPath = "resources/webharvest/itunes_apple.xml";
+	private final String tempPath = "data";
+	private final String linksFilePath = tempPath + "/" + "allLinks.txt";
+	private final String workingDir = System.getProperty("user.dir");
 
 	public ItunesWrapper() {
 	}
@@ -24,10 +25,11 @@ public class ItunesWrapper extends Wrapper {
 	private void wrapLinks(int maxloops) throws WebHarvestException {
 		try {
 			ScraperConfiguration config =
-					new ScraperConfiguration(configPath);
+					new ScraperConfiguration(wrapLinkConfigPath);
 			Scraper scraper = new Scraper(config, workingDir);
 			scraper.addVariableToContext("path", linksFilePath);
 			scraper.addVariableToContext("maxloops", maxloops);
+			System.out.println("Start. Wrapping links");
 			log.info("Start. Wrapping links");
 			scraper.execute();
 			log.info("End.Wrapping links");
@@ -40,16 +42,32 @@ public class ItunesWrapper extends Wrapper {
 		log.info("Start. Downloading links");
 		int i = 1;
 		Scanner sc = new Scanner(new File(linksFilePath));
+		//todo download only new links..  (hashset fro db)
 		while (sc.hasNext()) {
 			String url = sc.next();
 			String filePath = tempPath + "/" + i + ".html";
-			downloadOneLink(url, filePath );
+			downloadOneLink(url, filePath);
 			++i;
 		}
 		log.info("End. Downloading links");
 	}
 
-	private static void downloadOneLink(String urlStr, String filePath) throws Exception{
+	private void wrapPages() throws WebHarvestException {
+		try {
+			ScraperConfiguration config =
+					new ScraperConfiguration(wrapPageConfigPath);
+			Scraper scraper = new Scraper(config, workingDir);
+			scraper.addVariableToContext("path", tempPath);
+			log.info("Start. Wrapping pages");
+			scraper.execute();
+			log.info("End. Wrapping pages");
+		} catch (Exception exp) {
+			exp.printStackTrace();
+			//throw new WebHarvestException();
+		}
+	}
+
+	private static void downloadOneLink(String urlStr, String filePath) throws Exception {
 		log.debug("Downloading " + urlStr + " to " + filePath);
 		URL url = new URL(urlStr);
 		InputStream response = url.openStream();
@@ -64,10 +82,11 @@ public class ItunesWrapper extends Wrapper {
 		fw.close();
 	}
 
-	public void wrap(){
+	public void wrap() {
 		try {
-			wrapLinks(1);
+			//wrapLinks(1);
 			//downloadLinks();
+			wrapPages();
 		} catch (Exception exp) {
 			exp.printStackTrace();
 		}
